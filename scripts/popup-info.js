17780f60e39d978975c292882e612bf93649a3db
iamalonethesigmaone <iamalonethesigmaone@gmail.com>
2025-02-21 10:04:42 +0800

    membuat file belajar-shadow dari sekolah

---
+++ scripts/popup-info.js
@@ -0,0 +1,59 @@
+class gambarNadine extends HTMLElement{
+    constructor(){
+        super();
+
+        this._shadowakar = this.attachShadow({mode: "open"});
+
+        this._image = this.getAttribute('img');
+        this._imgcaption = this.getAttribute('imgcaption');
+        this._imgalt = this.getAttribute('imgalt');
+
+        this._style = document.createElement('style');
+}
+
+    connectedCallback(){
+        this.render();
+}
+
+    updateSyle(){
+        this._style.textContent = `
+        div{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 500px;
+        }
+        
+        img {
+            width: 150px;
+            height: 150px;
+            object-fit: cover;
+            object-position: center;
+        }
+
+        imgcaption{
+            width: 100%;
+            background-color: black;
+            color: white;
+            text-align: center;
+        }
+        `
+    }
+
+    render(){
+        this.updateSyle();
+
+        const template = `
+            ${this._style.outerHTML}
+
+            <div>
+                <img src="${this._image}" alt="${this._imgalt}">
+                <imgcaption>${this._imgcaption}</imgcaption>
+            </div>
+        `;
+
+        this._shadowakar.innerHTML = template;
+    }
+}
+
+window.customElements.define('gambar-nadine', gambarNadine);
